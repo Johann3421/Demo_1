63fb680997065b785af5ebe55876000e77ec8aec
Johann3421 <1355430@senati.pe>
2025-02-28 16:21:58 -0500

    Al fin lo logreeee

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,24 +8,37 @@
     <title>@yield('title', 'Mi Tienda')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
     <meta name="description" content="Explora nuestra selección de productos de alta calidad y encuentra lo que necesitas al mejor precio. Envíos rápidos y seguros en toda España.">
     <meta name="keywords" content="productos, tienda online, ecommerce, ofertas, comprar online, envío rápido, productos de calidad">
     <meta name="robots" content="index, follow">
     <link rel="icon" href="{{ asset('images/logo2.ico') }}" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    
     <!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
     @include('partials.header')
-    <main class="container mt-4">
-        @yield('content')
-    </main>
-    @include('partials.footer')
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                @include('partials.slider') <!-- Asegura que el slider no se vea afectado -->
+            </div>
+        </div>
+
+        <div class="row content-wrapper">
+            <aside class="col-md-3 d-none d-md-block left-sidebar">
+                @include('partials.aside')
+            </aside>
+            <main class="col-md-9 col-12">
+                @yield('content')
+            </main>
+        </div>
+    </div>
+
+    @include('partials.footer') <!-- Ahora siempre se mostrará correctamente -->
 </body>
+
 
 </html>
---
+++ resources/views/partials/aside.blade.php
@@ -0,0 +1,167 @@
+{{-- resources/views/partials/aside.blade.php --}}
+<div class="left-sidebar">
+    <h2>Categorias</h2>
+    <div class="panel-group category-products" id="accordian">
+        <!-- Categorías -->
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a href="index.php?categoria=ALMACENAMIENTO#ver" class="tech-link">
+                        <i class="fas fa-microchip tech-icon"></i>
+                        <span class="tech-text animate__animated animate__fadeIn">ALMACENAMIENTO</span>
+                    </a>
+                </h4>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a href="index.php?categoria=CAMARA DE SEGURIDAD Y ACCESOS#ver" class="tech-link">
+                        <i class="fas fa-microchip tech-icon"></i>
+                        <span class="tech-text animate__animated animate__fadeIn">CAMARA DE SEGURIDAD Y ACCESOS</span>
+                    </a>
+                </h4>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a href="index.php?categoria=COMPONENTES Y ACCESORIOS PARA LAPTOP#ver" class="tech-link">
+                        <i class="fas fa-microchip tech-icon"></i>
+                        <span class="tech-text animate__animated animate__fadeIn">COMPONENTES Y ACCESORIOS PARA LAPTOP</span>
+                    </a>
+                </h4>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a href="index.php?categoria=COMPONENTES Y ACCESORIOS PARA PC#ver" class="tech-link">
+                        <i class="fas fa-microchip tech-icon"></i>
+                        <span class="tech-text animate__animated animate__fadeIn">COMPONENTES Y ACCESORIOS PARA PC</span>
+                    </a>
+                </h4>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a href="index.php?categoria=COMPONENTES Y ACCESORIOS PARA PROYECTOR#ver" class="tech-link">
+                        <i class="fas fa-microchip tech-icon"></i>
+                        <span class="tech-text animate__animated animate__fadeIn">COMPONENTES Y ACCESORIOS PARA PROYECTOR</span>
+                    </a>
+                </h4>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a href="index.php?categoria=COMPONENTES Y ACCESORIOS PARA TABLET#ver" class="tech-link">
+                        <i class="fas fa-microchip tech-icon"></i>
+                        <span class="tech-text animate__animated animate__fadeIn">COMPONENTES Y ACCESORIOS PARA TABLET</span>
+                    </a>
+                </h4>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a href="index.php?categoria=IMPRESORA#ver" class="tech-link">
+                        <i class="fas fa-microchip tech-icon"></i>
+                        <span class="tech-text animate__animated animate__fadeIn">IMPRESORA</span>
+                    </a>
+                </h4>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a href="index.php?categoria=LAPTOP#ver" class="tech-link">
+                        <i class="fas fa-microchip tech-icon"></i>
+                        <span class="tech-text animate__animated animate__fadeIn">LAPTOP</span>
+                    </a>
+                </h4>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a href="index.php?categoria=PC-SERVIDOR-ALL IN ONE#ver" class="tech-link">
+                        <i class="fas fa-microchip tech-icon"></i>
+                        <span class="tech-text animate__animated animate__fadeIn">PC-SERVIDOR-ALL IN ONE</span>
+                    </a>
+                </h4>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a href="index.php?categoria=PERIFERICOS#ver" class="tech-link">
+                        <i class="fas fa-microchip tech-icon"></i>
+                        <span class="tech-text animate__animated animate__fadeIn">PERIFERICOS</span>
+                    </a>
+                </h4>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a href="index.php?categoria=PROTECCION ELECTRICA Y ENERGIA#ver" class="tech-link">
+                        <i class="fas fa-microchip tech-icon"></i>
+                        <span class="tech-text animate__animated animate__fadeIn">PROTECCION ELECTRICA Y ENERGIA</span>
+                    </a>
+                </h4>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a href="index.php?categoria=PROYECTOR#ver" class="tech-link">
+                        <i class="fas fa-microchip tech-icon"></i>
+                        <span class="tech-text animate__animated animate__fadeIn">PROYECTOR</span>
+                    </a>
+                </h4>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a href="index.php?categoria=REDES Y CONETIVIDAD#ver" class="tech-link">
+                        <i class="fas fa-microchip tech-icon"></i>
+                        <span class="tech-text animate__animated animate__fadeIn">REDES Y CONETIVIDAD</span>
+                    </a>
+                </h4>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a href="index.php?categoria=SOFTWARE-LICENCIAS#ver" class="tech-link">
+                        <i class="fas fa-microchip tech-icon"></i>
+                        <span class="tech-text animate__animated animate__fadeIn">SOFTWARE-LICENCIAS</span>
+                    </a>
+                </h4>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a href="index.php?categoria=SUMINISTROS PARA IMPRESORA#ver" class="tech-link">
+                        <i class="fas fa-microchip tech-icon"></i>
+                        <span class="tech-text animate__animated animate__fadeIn">SUMINISTROS PARA IMPRESORA</span>
+                    </a>
+                </h4>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a href="index.php?categoria=TABLET TABLETA#ver" class="tech-link">
+                        <i class="fas fa-microchip tech-icon"></i>
+                        <span class="tech-text animate__animated animate__fadeIn">TABLET TABLETA</span>
+                    </a>
+                </h4>
+            </div>
+        </div>
+    </div>
+</div>
